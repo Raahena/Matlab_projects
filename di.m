6f23ce40da810cef97476b378f4e7e147223ce2f
Raahena <118992172+Raahena@users.noreply.github.com>
2025-03-24 15:54:52 +0530

    diameter.m

--- di.m
+++ di.m
@@ -1,11 +1,62 @@
-rgb = imread("im.jpeg");
-imshow(rgb);
-d = drawline;
-pos = d.Position;
-diffPos = diff(pos);
-diameter = hypot(diffPos(1),diffPos(2));
-imshow(rgb)
-h = viscircles(centers,radii);
-gray_image = im2gray(rgb);
-imshow(gray_image);
-[centers,radii] = imfindcircles(rgb,[20 25],ObjectPolarity="dark",Sensitivity=0.9);
+clc;
+clear all;
+
+
+img =imread('bbb.jpg');
+imshow(img);
+
+red=img(:,:,1);
+green=img(:,:,2);
+blue=img(:,:,3);
+
+figure(1);
+subplot(2,2,1);
+imshow(img);
+title('Original image');
+subplot(2,2,2);
+imshow(red);
+title('Red plane');
+subplot(2,2,3);
+imshow(green);
+title('Green plane');
+subplot(2,2,4);
+imshow(blue);
+title('Blue plane');
+
+figure(2);
+level=0.57;
+bw2=im2bw(blue,level);
+subplot(2,2,1);
+imshow(bw2);
+title('Blue plane threshold');
+
+fill=imfill(bw2,'holes');
+subplot(2,2,2);
+imshow(fill);
+title('holes filled');
+
+clearimg=imclearborder(fill);
+subplot(2,2,3);
+imshow(clearimg);
+title('Remove blobs on border');
+se=strel('disk',7);
+open=imopen(fill,se);
+subplot(2,2,4);
+imshow(open);
+title('Remove small blobs');
+
+
+diameter=regionprops(open,'MajorAxisLength');
+
+
+diameterPixels = diameter(1).MajorAxisLength;
+
+if ~isempty(diameter)
+    disp(['Measured Diameter of the object: ', num2str(diameterPixels), ' pixels']);
+else
+    disp('No objects detected.');
+end
+
+figure(3);
+imshow(img);
+d=imdistline;
